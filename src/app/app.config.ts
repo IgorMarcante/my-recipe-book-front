63bc9cdba7f934b6e5dc40e196343fe9df08f3c6
Igor Marcante <igormarcante@hotmail.com>
2025-05-03 16:06:49 -0400

    componente de loading

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,13 +1,12 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { routes } from './app.routes';
-import { provideHttpClient } from '@angular/common/http';
-import { provideClientHydration } from '@angular/platform-browser';
+import { loadingInterceptor } from './interceptors/loading.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
-    provideHttpClient(),
-    provideClientHydration()
+    provideHttpClient(withInterceptors([loadingInterceptor]))
   ]
 };
---
+++ src/app/interceptors/loading.interceptor.ts
@@ -0,0 +1,26 @@
+// src/app/interceptors/loading.interceptor.ts
+import { HttpInterceptorFn } from '@angular/common/http';
+import { inject } from '@angular/core';
+import { finalize } from 'rxjs/operators';
+import { LoadingService } from '../services/loading.service';
+
+export const loadingInterceptor: HttpInterceptorFn = (req, next) => {
+  const loadingService = inject(LoadingService);
+  let activeRequests = 0;
+
+  setTimeout(() => {
+    if (activeRequests === 0) {
+      loadingService.show();
+    }
+    activeRequests++;
+  }, 0);
+
+  return next(req).pipe(
+    finalize(() => {
+      activeRequests--;
+      if (activeRequests === 0) {
+        loadingService.hide();
+      }
+    })
+  );
+};
